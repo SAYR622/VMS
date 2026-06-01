b9913ce5136969f343905579c8a170ceabd80568
Yashod Randhinu <sayrandhinu@students.nsbm.ac.lk>
2026-06-02 01:46:13 +0530

    Converting from windows from to WPF Application (#18)
    * Add files via upload
    
    * Delete CHSMS (Windows Form) directory
    
    * Delete CHSMS (WPF Application) directory
    
    * Add files via upload

---
+++ CHSMS (WPF Application)/CHSMS/DatabaseConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CHSMS
+{
+    public static class DatabaseConfig
+    {
+        // Paste your exact copied string inside the quotes below
+        public static string ConnectionString = @"Server=(localdb)\MSSQLLocalDB;Database=CHSMS;Trusted_Connection=True;";
+    }
+}
---
+++ CHSMS (WPF Application)/CHSMS/login.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace CHSMS
+{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class login : Window
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(DatabaseConfig.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // FIXED: Changed to MessageBoxButton and MessageBoxImage
+                    MessageBox.Show("Connection to the database was successful!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    // Put your SELECT query and password checking logic here!
+                }
+                catch (Exception ex)
+                {
+                    // FIXED: Changed to MessageBoxButton and MessageBoxImage
+                    MessageBox.Show("Database connection error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
